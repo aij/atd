0b576a691aea1bdfc6295c8754eb57c0164e857b
Ivan Jager <aij+git@mrph.org>
2018-09-22 14:36:38 -0500

    wip atds

---
+++ atds/test/run_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CLASSPATH='.:json.jar:junit-4.8.2.jar'
+
+scalac -classpath $CLASSPATH com/mylife/test/*.scala
+javac -classpath $CLASSPATH AtdjTest.java
+#javadoc -quiet -Xdoclint:none -classpath $CLASSPATH -d doc/test \
+#        -public com.mylife.test -quiet \
+#    | grep -v "Creating destination directory"
+#java  -classpath $CLASSPATH AtdjTest | grep -v -E '^(Time:|JUnit version)' > java.trace
